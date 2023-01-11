e92d1038080f6cefdab8b2c38a6db14bc2882073
this-is-yaash <yashwanth1715@gmail.com>
2023-01-11 17:58:56 +0530

    inputwithJS

--- Frontend/Admin/login.js
+++ Frontend/Admin/login.js
@@ -1,5 +1,7 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        
-    });
-});
+function lgn() {
+    if ($("#exampleInputEmail1").val() == "y@gmail.com" && $("#exampleInputPassword1").val() == "12345") {
+        window.location.replace("emp_attendance.html");
+    } else {
+      alert("You are not a valid user");
+    }
+  }
